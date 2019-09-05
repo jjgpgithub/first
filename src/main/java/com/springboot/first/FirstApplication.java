ef6b8edbcfa194974e6e25429a3b88d47d33301a
jjgp <jjgpmail@qq.com>
2019-09-06 01:26:41 +0800

    初始化2

--- src/main/java/com/springboot/first/FirstApplication.java
+++ src/main/java/com/springboot/first/FirstApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class FirstApplication {
+
     @RequestMapping("/")
     String index() {
         return "Hello Spring Boot";
