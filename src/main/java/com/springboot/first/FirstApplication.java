920a1cce3fedb246b9eaae380c01e406b2840f89
jjgp <jjgpmail@qq.com>
2019-09-08 14:12:00 +0800

    master  11 dev 1

--- src/main/java/com/springboot/first/FirstApplication.java
+++ src/main/java/com/springboot/first/FirstApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstApplication {
 
 //maste1
+    //master2
     @RequestMapping("/")
     String index() {
         System.out.println("jiangjianggongping");
